734f43e76a567da28116a82f52f4eebcd0f6ea4f
soeun <ddonydev@gmail.com>
2024-01-30 18:56:12 +0900

    Time: 23 ms (67.64%), Space: 46.7 MB (80.47%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -4,9 +4,8 @@ class Solution {
 
         Arrays.sort(products);
 
-        
+        List<String> list = new ArrayList<>();
         for (int i = 0; i < searchWord.length(); i++) {
-            List<String> list = new ArrayList<>();
             String prefix = searchWord.substring(0, i + 1);
             for (int j = 0; j < products.length; j++) {
                 if (products[j].startsWith(prefix)) {
@@ -17,6 +16,7 @@ class Solution {
                 }
             }
             answer.add(new ArrayList<>(list));
+            list.clear();
         }
         return answer;
     }
