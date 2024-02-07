754b55c31f719ffba5e7192927adedbf5ade4f05
soeun <ddonydev@gmail.com>
2024-02-07 15:06:23 +0900

    Time: 6 ms (16.01%), Space: 42.3 MB (28.16%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] str = s.split(" ");
+        String answer = "";
+        for (int i = 0; i < k; i++) {
+            if (i == k - 1) {
+                answer += str[i];
+            }else{
+                answer += str[i] + " ";
+            }
+        }
+        return answer;
+    }
+}
