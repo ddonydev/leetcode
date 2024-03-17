0c34544783c7030cbebffd0019580e39ebd32c92
Soeun <ddonydev@gmail.com>
2024-03-17 18:47:13 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (56.91%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+
+        int sum = arrivalTime + delayedTime;
+        if(sum >= 24){
+            sum = sum - 24;
+        }
+        return sum;
+    }
+}
