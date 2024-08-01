13babba5a03b52c8badde9f90c3e182ccb1b86ba
Soeun <ddonydev@gmail.com>
2024-08-01 22:54:18 +0900

    Time: 1 ms (82.64%), Space: 42.7 MB (34.64%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -5,8 +5,10 @@ class Solution {
         for(int i = 0; i < details.length; i++){
             char a = details[i].charAt(11);
             char b = details[i].charAt(12);
+            
+            String str = details[i].substring(11, 13);
 
-            if(Integer.parseInt(String.valueOf(a) + String.valueOf(b)) > 60){
+            if(Integer.parseInt(str) > 60){
                 cnt++;
             }
         }
